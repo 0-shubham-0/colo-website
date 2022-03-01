dcb6c43881f3aaf0ec286483bc7bd07d75daf6b8
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-03-01 20:21:32 +0530

    fixed speakers

--- event.js
+++ event.js
@@ -5,4 +5,13 @@ dates.forEach(n => n.addEventListener("click", ClickHandler));
 function ClickHandler(){
     dates.forEach(n => n.classList.remove('active'));
     this.classList.add('active');
+    let filter = this.getAttribute('data-filter');
+    rows.forEach(row => {
+        row.classList.add('hide');
+        row.classList.remove('active');
+        if(row.getAttribute('data-item')== filter|| filter=="All"){
+            row.classList.add('active'); 
+            row.classList.remove('hide'); 
+        }
+    });
 }
