6b45d6c6e1b48eaacd0fd976806f642100f79691
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-03-01 23:36:38 +0530

    added countdown timer

--- nav.js
+++ nav.js
@@ -30,3 +30,26 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navlinks.classList.remove("active");
 }
+
+
+const countdown = () =>{
+    const newDate = new Date('March 6, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    let gap = newDate - now;
+
+    const second = 1000;
+    const minute = second*60;
+    const hour = minute*60;
+    const day = hour*24;
+    
+    const textDay = Math.floor(gap/day);
+    const textHour = Math.floor((gap%day)/hour);
+    const textMinute = Math.floor((gap%hour)/minute);
+    const textSecond = Math.floor((gap%minute)/second);
+    document.querySelector(".days").innerText = textDay;
+    document.querySelector(".hours").innerText = textHour;
+    document.querySelector(".minutes").innerText = textMinute;
+    document.querySelector(".seconds").innerText = textSecond;
+  }
+  
+setInterval(countdown,1000);
