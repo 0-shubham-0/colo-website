34b94d86dc5a0e3e7eccb65982e7f08e55ddfb2b
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2022-03-23 21:52:10 +0530

    .

--- event.js
+++ event.js
@@ -17,3 +17,17 @@ function ClickHandler(){
     });
 }
 
+var allRadios = document.getElementsByName('toggle');
+        var booRadio;
+        var x = 0;
+        for(x = 0; x < allRadios.length; x++){
+
+            allRadios[x].onclick = function() {
+                if(booRadio == this){
+                    this.checked = false;
+                    booRadio = null;
+                }else{
+                    booRadio = this;
+                }
+            };
+        }
