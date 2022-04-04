68faf0fd9120434896e7dbfe9febd2ddad1d482d
Alston <0508alstonfernandes@gmail.com>
2022-04-05 00:36:17 +0530

    updated time

--- event.js
+++ event.js
@@ -40,3 +40,63 @@ var allRadios = document.getElementsByName('toggle');
 //         clearInterval(refreshIntervalId);
 //     }
 // }, millisecs);
+
+
+var duration = 30 * 1000;
+var end = Date.now() + duration;
+let Day =document.querySelector(".days");
+let Hour = document.querySelector(".hours");
+let Minute = document.querySelector(".minutes");
+let Second = document.querySelector(".seconds");
+const countdown = () => {
+    const newDate = new Date("April 05, 2022 10:30:00").getTime();
+    const now = new Date().getTime();
+    let gap = newDate - now;
+  
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+    Day.innerText = textDay;
+    Hour.innerText = textHour;
+    Minute.innerText = textMinute;
+    Second.innerText = textSecond;
+    if(Day.innerText<=0 && Hour.innerText<=0 && Minute.innerText<=0 && Second.innerText<=0){
+        Day.innerText=0; 
+        Hour.innerText=0;
+        Minute.innerText=0;
+        Second.innerText=0;
+    }
+  };
+  
+  setInterval(countdown, 1000);
+
+  
+// (function frame() {
+//   // launch a few confetti from the left edge
+//   confetti({
+//     particleCount: 55,
+//     angle: 60,
+//     spread: 95,
+//     origin: { x: 0 }
+//   });
+//   // and launch a few from the right edge
+//   confetti({
+//     particleCount:55,
+//     angle: 120,
+//     spread: 95,
+//     origin: { x: 1 }
+//   });
+
+//   // keep going until we are out of time
+//   if (Date.now.getTime()== newDate.getTime()){ 
+//     if(Date.now() < end) {
+//         requestAnimationFrame(frame);
+//     }
+//     }
+// }());
--- nav.js
+++ nav.js
@@ -27,29 +27,6 @@ function closeMenu() {
   }, 600);
   hamburger.classList.remove("active");
 }
-
-const countdown = () => {
-  const newDate = new Date("April 05, 2022 10:30:00").getTime();
-  const now = new Date().getTime();
-  let gap = newDate - now;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
-  document.querySelector(".days").innerText = textDay;
-  document.querySelector(".hours").innerText = textHour;
-  document.querySelector(".minutes").innerText = textMinute;
-  document.querySelector(".seconds").innerText = textSecond;
-};
-
-setInterval(countdown, 1000);
-
 hamburger.addEventListener("click", () => {
   if (hamburger.classList.contains("active")) {
     navlinks.classList.remove("active");
